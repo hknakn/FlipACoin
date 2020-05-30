2f37253dbef2a207aa59e345f5e764fec498b54c
Hakan Akın <cehakanakin@gmail.com>
2020-05-30 23:11:05 +0300

    Created project files

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, StatusBar} from 'react-native';
+import MainScreen from './src/screens/mainScreen';
+
+const App = () => {
+  return <MainScreen />;
+};
+
+export default App;
